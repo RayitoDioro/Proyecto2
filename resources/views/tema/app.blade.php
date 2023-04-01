f52d0af706dccb8ad20fc840e8062413647d8c99
RayitoDioro <carlosth54321@gmail.com>
2023-03-31 23:11:15 -0600

    Proyecto 2

--- resources/views/tema/app.blade.php
+++ resources/views/tema/app.blade.php
@@ -6,10 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>@yield('title')</title>
+
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     @livewireStyles
 </head>
+
 <body class="bg-light">
     <div class="container shadow bg-white">
         <div class="row">
@@ -17,14 +20,24 @@
                 <h1>Formularios</h1>
             </div>
             <div class="col-sm-12">
+
+            <form method="POST" action="{{ route('logout') }}">
+              @csrf
+              <button type="submit" class="session">Cerrar sesión</button>
+
+            </form>
                 <a href="{{ route('tarea.create') }}" class="btn btn-link" >Crear nueva tarea</a>
                 <a href="{{ route('tarea.index') }}" class="btn btn-link" >Listar tareas</a>
+
+                
             </div>
             <div class="col-sm-12">
                 @yield('contenido')
             </div>
         </div>
     </div>
+
+    
     @livewireScripts
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\TareaController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,25 +11,46 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/formularios', function () {
-    return view('formularios');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 });
 
-/*Route::get('tarea/{tarea}/ver', [TareaController::class, 'show'])->name('tarea.show');
-Route::get('tarea/registrar', [TareaController::class, 'create'])->name('tarea.create');
-Route::post('tarea/guardar', [TareaController::class, 'store'])->name('tarea.store');
-Route::get('tarea/listar', [TareaController::class, 'index'])->name('tarea.index');
-Route::get('tarea/{tarea}/editar', [TareaController::class, 'edit'])->name('tarea.edit');
-Route::put('tarea/{tarea}/actualizar', [TareaController::class, 'update'])->name('tarea.update');
-Route::delete('tarea/{tarea}/eliminar', [TareaController::class, 'destroy'])->name('tarea.destroy');
-*/
-Route::resource('tarea', TareaController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::get('tarea/{tarea}/ver', [TareaController::class, 'show'])->name('tarea.show');
+    Route::get('tarea/registrar', [TareaController::class, 'create'])->name('tarea.create');
+    Route::post('tarea/guardar', [TareaController::class, 'store'])->name('tarea.store');
+    Route::get('tarea/listar', [TareaController::class, 'index'])->name('tarea.index');
+    Route::get('tarea/{tarea}/editar', [TareaController::class, 'edit'])->name('tarea.edit');
+    Route::put('tarea/{tarea}/actualizar', [TareaController::class, 'update'])->name('tarea.update');
+    Route::delete('tarea/{tarea}/eliminar', [TareaController::class, 'destroy'])->name('tarea.destroy');
+    });
+
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified',
+    ])->group(function () {
+        Route::get('/dashboard', function () {
+            return redirect()->route('tarea.index');
+        })->name('dashboard');
+    });
