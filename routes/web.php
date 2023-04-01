bc99938b9bdd9c41218d740d2bbc01fa925bfc36
RayitoDioro <carlosth54321@gmail.com>
2023-03-31 21:26:14 -0600

    Rutas corregidas (web.php)

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,12 @@ Route::get('/formularios', function () {
     return view('formularios');
 });
 
-Route::get('tarea/{tarea}/ver', [TareaController::class, 'show'])->name('tarea.show');
+/*Route::get('tarea/{tarea}/ver', [TareaController::class, 'show'])->name('tarea.show');
 Route::get('tarea/registrar', [TareaController::class, 'create'])->name('tarea.create');
 Route::post('tarea/guardar', [TareaController::class, 'store'])->name('tarea.store');
 Route::get('tarea/listar', [TareaController::class, 'index'])->name('tarea.index');
 Route::get('tarea/{tarea}/editar', [TareaController::class, 'edit'])->name('tarea.edit');
 Route::put('tarea/{tarea}/actualizar', [TareaController::class, 'update'])->name('tarea.update');
 Route::delete('tarea/{tarea}/eliminar', [TareaController::class, 'destroy'])->name('tarea.destroy');
+*/
+Route::resource('tarea', TareaController::class);
